4bac1e943c994c522c59844f9a8e0c99cce8002d
Fatih Tasdemir <tsdmrfth@gmail.com>
2021-12-23 23:53:20 +0300

    Finish project.

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,88 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import twitterLogo from './assets/twitter-logo.svg'
+import CandyMachine from './CandyMachine'
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = 'CChiklot'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+
+// candy machine pubkey: DMZLBHYzWQHEUaeYG3yFeRzxwgbXyz25FQjbGFpxd7Fp
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
-};
+    const [walletAddress, setWalletAddress] = useState(null)
 
-export default App;
+    useEffect(() => {
+        const onLoad = async () => {
+            await checkIfWalletIsConnected()
+        }
+        window.addEventListener('load', onLoad)
+        return () => window.removeEventListener('load', onLoad)
+    }, [])
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { solana } = window
+
+            if (solana) {
+                if (solana.isPhantom) {
+                    const response = await solana.connect({ onlyIfTrusted: true })
+                    setWalletAddress(response.publicKey.toString())
+                }
+            } else {
+                alert('Solana object not found! Get a Phantom Wallet 👻')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const renderContent = () => {
+        if (walletAddress) {
+            return (
+                <CandyMachine walletAddress={window.solana} />
+            )
+        }
+
+        return (
+            <button
+                onClick={connectWallet}
+                className="cta-button connect-wallet-button">
+                Connect to Wallet
+            </button>
+        )
+    }
+
+    const connectWallet = async () => {
+        const { solana } = window
+
+        if (solana) {
+            const response = await solana.connect()
+            console.log('Connected with Public Key:', response.publicKey.toString())
+            setWalletAddress(response.publicKey.toString())
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <p className="header">🐈 Cat NFTs</p>
+                    <p className="sub-text">NFT drop machine with fair mint</p>
+                    {renderContent()}
+                </div>
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo}/>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={TWITTER_LINK}
+                        className="footer-text">
+                        {`built by @${TWITTER_HANDLE}`}
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
